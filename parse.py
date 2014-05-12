c34b3cf113fa491f7779e28138f5447ec16226d4
areckx <alex.loranger.1988@gmail.com>
2014-05-12 21:14:04 +0000

    incremental changes

--- parse.py
+++ parse.py
@@ -1,55 +1,34 @@
-"""
-Data Visualization Project
-
-Parse data from an ugly CSV or Excel file, and render it in
-JSON, save to a database, and visualize in graph form.
-
-Part I: Taking data from a CSV/Excel file, and return it into a format
-that is easier for Python to play with.
-
-Copyright (c) 2013 E. Lynn Root
-Distributed under the zlib png license. See LICENSE for details.
-"""
-
+# -*- coding: utf-8 -*-
+#!/usr/bin/python2
+#// File: parse.py
+#// Description: [ parses a csv file and represents its data in a graph ]
+#// Current Version: 0.1-0 as of 2014-mm-dd written by Alex Loranger
+#// This comment has ceased to be!
+#//
 import csv
 
+MY_FILE = "data/sample_sfpd_incident_all.csv"
 
-# Put the full path to your CSV/Excel file here
-MY_FILE = "../data/sample_sfpd_incident_all.csv"
-
-
+# In defining the function, we know that we want it to give a CSV file, as well as
+# the delimiter in which the CSV file uses to delimit each element/column
 def parse(raw_file, delimiter):
-    """Parses a raw CSV file to a JSON-like object"""
+    """Parses a raw CSV file to a JSON-line object."""
 
-    # Open CSV file, and safely close it when we're done
+# We also know that we want to return a JSON-like object. A JSON file/object is just 
+# a collection of dictionaries, much like Python's dictionary
+
+    # Open CSV file
     opened_file = open(raw_file)
-    
-    # Read the CSV data
-    csv_data = csv.reader(opened_file, delimiter=delimiter)
 
-    # Setup an empty list
-    parsed_data = []
+    # Read CSV file
+    ## second `delimiter` refers to parse() argument
+    csv_data = csv.reader(opened_file, delimiter = delimiter)
 
-    # Skip over the first line of the file for the headers
-    fields = csv_data.next()
+    # Close CSV file
 
-    # Iterate over each row of the csv file, zip together field -> value
-    for row in csv_data:
-        parsed_data.append(dict(zip(fields, row)))
-
-    # Close the CSV file
-    opened_file.close()
+    # Build a data structure to return parsed data
 
     return parsed_data
 
 
-def main():
-    # Call our parse function and give it the needed parameters
-    new_data = parse(MY_FILE, ",")
-
-    # Let's see what the data looks like!
-    print new_data
-
-
-if __name__ == "__main__":
-    main()
+#EOF
