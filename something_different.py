bd369d3d877d656ce89e7e909f79694c70ce7a61
areckx <alex.loranger.1988@gmail.com>
2014-05-12 20:16:47 +0000

    add python 2 template file

---
+++ something_different.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python2
+#// File: something_different.py
+#// Description: [ ]
+#// Current Version: 0.1-0 as of 2014-mm-dd written by Alex Loranger
+#// This comment has ceased to be!
+#//
+
+
+#EOF
