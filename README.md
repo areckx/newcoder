90f6e794434908409aa1b509b334f3714e503feb
areckx <areckx@localhost.localdomain>
2014-05-12 15:50:28 +0000

    testing

--- README.md
+++
@@ -1,4 +0,0 @@
-newcoder
-========
-
-newcoder.io tutorials
--- spam_eggs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-#// File: spam_eggs.py
-#// Description: [ ]
-#// Current Version: 0.1-0 as of 2014-mm-dd written by Alex Loranger
-#// This comment has ceased to be!
-#//
-
-
-#EOF
